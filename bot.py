317069a9caeacfcce75b7d938506c4b64ef6ec63
Yasmine091 <7486279-Yasmine091@users.noreply.gitlab.com>
2021-03-06 21:12:52 +0100

    ajout chant basique

--- bot.py
+++ bot.py
@@ -19,9 +19,13 @@ async def on_message(message):
     sad_answers = ['¡Ánimo, no te rindas!', '¿Qué te ocurre?']
     happy = ['contenta', 'contento' 'feliz', 'alegria', 'alivio', 'felicidad']
     happy_answers = ['¡Me alegro por tí!', '¡Que bien!', 'Enhorabuena por lo que sea que hayas conseguido, tu felicidad es la nuestra XD']
+    sing = ['canta', 'cantar', 'canta!', 'cantar!']
 
     if message.content.startswith('$hola'):
         await message.channel.send('¡Hola!')
+
+    if any(word in msg for word in sing):
+        await message.channel.send('¡PRRRIIIIIIIII PIPIPIPI PIO PIO PIO PIO PI PI PI, PIIII PIIII PIIII!')
     
     if any(word in msg for word in sad):
         await message.channel.send(random.choice(sad_answers))
@@ -30,4 +34,4 @@ async def on_message(message):
         await message.channel.send(random.choice(happy_answers))
 
 
-client.run(os.getenv('TOKEN'))
+# client.run(os.getenv('TOKEN'))
